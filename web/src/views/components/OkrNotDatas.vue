4d8ca5533e6e607600f99c84a662f5af35b6d80a
weifashi <605403358@qq.com>
2023-07-26 14:50:55 +0800

    style：调整代码格式

--- web/src/views/components/OkrNotDatas.vue
+++ web/src/views/components/OkrNotDatas.vue
@@ -1,7 +1,7 @@
 <template>
     <div class=" w-full h-full flex mt-[10%] items-center flex-col">
         <div>
-            <img class="mr-6 -mt-2" src="@/assets/images/icon/okrNotData.svg" />
+            <img class="mr-6 -mt-2" src="@/assets/images/icon/notData.svg" />
         </div>
         <div class="text-[#515A6E] text-14 text-opacity-50 text-center">        
             <slot name="content"></slot>
