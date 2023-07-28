ffd290f848580509785cc88b0106a4e3c5308b7d
hitosea-61 <2622788078@qq.com>
2023-07-28 18:14:35 +0800

    fix：修改样式，去除不必要的代码

--- web/src/views/manage/OkrDepartment.vue
+++ web/src/views/manage/OkrDepartment.vue
@@ -2,68 +2,55 @@
     <n-scrollbar>
         <div class="okr-department-main">
             <OkrLoading v-if="loadIng"></OkrLoading>
-            <div v-else>
-                <div class=" flex items-center mb-16 mt-0">
+            <div class=" flex items-center mb-16 mt-0 nowrap">
 
-                    <div class=" mb-2 mr-8">
-                        {{$t('部门')}}
-                    </div>
-                    <n-select 
-                        v-model:value="departmentsvalue" 
-                        :options="departments" 
-                        class="custom-select" 
-                        placeholder="全部"/>
-
-                    <div class=" mb-2 mr-8 ml-24">
-                        {{$t('负责人')}}
-                    </div>
-                    <n-select 
-                        v-model:value="principalvalue" 
-                        :options="principal" 
-                        class="custom-select" 
-                        placeholder="全部"/>
-
-                    <div class=" mb-2 mr-8 ml-24">
-                        {{$t('类型')}}
-                    </div>
-                    <n-select 
-                        v-model:value="typevalue" 
-                        :options="types" 
-                        class="custom-select" 
-                        placeholder="全部"/>
-
-                    <div class=" mb-2 mr-8 ml-24">
-                        {{$t('时间')}}
-                    </div>
-                    <n-date-picker class="custom-select" v-model:value="daterange"
-                    value-format="yyyy.MM.dd HH:mm:ss" type="daterange" clearable size="medium" placeholder="请选择时间"/>
-  
-                    <n-button type="primary" class=" mb-4 ml-24 rounded" @click="getList('search')">
-                        <i class="taskfont mr-5">&#xe72a;</i>
-                        {{ $t('搜索') }}  
-                    </n-button>
-                    <n-checkbox v-model:checked="completednotrated" class="ml-auto rounded" @click="getList('search')">
-                        {{ $t('已完成未评分') }}    
-                    </n-checkbox>             
+                <div class=" mb-2 mr-8  custom-div">
+                    {{$t('部门')}}
                 </div>
-                <OkrItems :list="list" v-if="list.length != 0"></OkrItems>
-                <OkrNotDatas v-else :msg="$t('暂无OKR')">
-                    <template v-slot:content>
-                        <div class="mt-5">
-                            <div class="mb-10"></div>     
-                            <div>
-                                <n-button type="primary" ghost>
-                                    <i class="taskfont mr-5">&#xe731;</i>
-                                    {{ $t('创建OKR') }}
-                                </n-button>
-                            </div>    
-                        </div>
-                    </template>
-                </OkrNotDatas>
+                <n-select 
+                    v-model:value="departmentsvalue" 
+                    :options="departments" 
+                    class="custom-select" 
+                    :placeholder="$t('全部')"/>
+
+                <div class=" mb-2 mr-8 ml-24 custom-div">
+                    {{$t('负责人')}}
+                </div>
+                <n-select 
+                    v-model:value="principalvalue" 
+                    :options="principal" 
+                    class="custom-select" 
+                    :placeholder="$t('全部')"/>
+
+                <div class=" mb-2 mr-8 ml-24 custom-div">
+                    {{$t('类型')}}
+                </div>
+                <n-select 
+                    v-model:value="typevalue" 
+                    :options="types" 
+                    class="custom-select" 
+                    :placeholder="$t('全部')"/>
+
+                <div class=" mb-2 mr-8 ml-24 custom-div">
+                    {{$t('时间')}}
+                </div>
+                <n-date-picker class="custom-select" v-model:value="daterange"
+                value-format="yyyy.MM.dd HH:mm:ss" type="daterange" clearable size="medium" :placeholder="$t('请选择时间')"/>
+
+                <n-button type="primary" class=" mb-4 ml-24 rounded" @click="getList('search')">
+                    <i class="taskfont mr-5">&#xe72a;</i>
+                    {{ $t('搜索') }}  
+                </n-button>
+                <n-checkbox v-model:checked="completednotrated" class="ml-auto rounded custom-div" @click="getList('search')">
+                    {{ $t('已完成未评分') }}    
+                </n-checkbox>             
             </div>
+            <OkrItems :list="list" v-if="list.length != 0"></OkrItems>
+            <OkrNotDatas v-else :msg="$t('暂无OKR')"></OkrNotDatas>
         </div>
     </n-scrollbar>
 </template>
+
 <script lang="ts" setup>
 import OkrItems from '@/views/components/OkrItems.vue'
 import { getDepartmentOkrList } from '@/api/modules/department' 
@@ -81,15 +68,15 @@ const departmentsvalue = ref(null)
 const principalvalue = ref(null)
 const typevalue = ref(null)
 const departments = ref([
-    { label: "全部", value: null },
+    { label: $t('全部'), value: null },
 ])
 const principal = ref([
-    { label: "全部", value: null },
+    { label: $t('全部'), value: null },
 ])
 const types = ref([
-    { label: "全部", value: null },
-    { label: "承诺型", value: "1" },
-    { label: "挑战型", value: "2" },
+    { label: $t('全部'), value: null },
+    { label: $t('承诺型'), value: "1" },
+    { label: $t('挑战型'), value: "2" },
 ]);
 const daterange = ref(null)
 const completednotrated = ref(false)
@@ -168,6 +155,9 @@ onMounted(() => {
     width: 160px;
     height: 36px;
 }
-
+.custom-div{
+    white-space: nowrap;
+    
+}
 
 </style>
