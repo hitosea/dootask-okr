d5729f9fc39538cbdec7864e13df3f358a0ac0ec
lintieyong <2622788078@qq.com>
2023-08-10 15:48:32 +0800

    fix：修改搜索无结果时提示文字

--- web/src/views/components/OkrNotDatas.vue
+++ web/src/views/components/OkrNotDatas.vue
@@ -6,7 +6,11 @@
         <div v-else>
             <img class="mr-6 -mt-2 w-80" src="@/assets/images/icon/notData.svg" />
         </div>
-        <div class="text-[#515A6E] text-14 text-opacity-50 text-center"> 
+        <div v-if="props.types!=''" class="text-[#515A6E] text-14 text-opacity-50 text-center"> 
+            <div class="mb-10" v-if="props.msg">{{ $t('没有找到匹配的结果') }}</div>           
+            <slot name="content"></slot>
+        </div>
+        <div v-else class="text-[#515A6E] text-14 text-opacity-50 text-center"> 
             <div class="mb-10" v-if="props.msg">{{props.msg}}</div>           
             <slot name="content"></slot>
         </div>
