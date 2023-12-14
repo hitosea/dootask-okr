ecdbf55decf6ed3ac07b1c901fe530f9102f07ee
HEXIANG <869411657@qq.com>
2023-12-14 16:12:33 +0800

    fix：复盘搜索

--- web/src/views/manage/OkrReplay.vue
+++ web/src/views/manage/OkrReplay.vue
@@ -1,6 +1,67 @@
 <template>
     <n-scrollbar :on-scroll="onScroll">
         <div class="okr-replay-main">
+
+            <div class="flex flex-wrap flex-col 2xl:flex-row 2xl:items-center ">
+                <div class="hidden md:flex items-center mb-16  min-w-full">
+                    <div class="flex-1 flex items-center overflow-hidden" v-if="userInfo == 'admin'">
+                        <div class="mb-2 mr-8 text-text-li whitespace-nowrap">
+                            {{ $t('部门') }}
+                        </div>
+                        <n-select v-model:value="departmentsvalue" :options="departments" clearable
+                            class="mr-16 flex-1 overflow-hidden" :placeholder="$t('全部')" />
+                    </div>
+
+                    <div class=" flex items-center overflow-hidden" :class="userInfo == 'admin' ? 'flex-1' : ''">
+                        <div class="text-text-li mr-8 whitespace-nowrap">
+                            {{ $t('负责人') }}
+                        </div>
+                        <n-select v-model:value="principalvalue" :options="principal" :on-search="getUser"
+                            :class="userInfo == 'admin' ? '' : 'max-w-[225px] '" class="flex-1 overflow-hidden" filterable
+                            :placeholder="$t('全部')" clearable>
+                            <template #action>
+                                <div v-if="principallast_page > principalpage" quaternary
+                                    class=" h-full w-full whitespace-nowrap text-center" @click.stop="principalClick('')">
+                                    {{ $t('更多...') }}
+                                </div>
+                                <div v-else quaternary class=" h-full w-full whitespace-nowrap text-center">
+                                    {{ $t('已经到底了') }}
+                                </div>
+                            </template>
+                        </n-select>
+                    </div>
+
+                    <div class="flex items-center" :class="userInfo == 'admin' ? 'flex-1' : ''">
+                        <div class="mr-8 ml-16 whitespace-nowrap text-text-li">{{ $t('时间') }}</div>
+                        <div v-if="showDatePickers" :class="userInfo == 'admin' ? '' : 'max-w-[225px] '"
+                            class="okr-date-picker-waps ">
+                            <DatePickers />
+                        </div>
+                        <n-date-picker v-else :class="userInfo == 'admin' ? '' : 'max-w-[225px] '" v-model:value="daterange"
+                            value-format="yyyy.MM.dd HH:mm:ss" type="daterange" clearable size="medium" />
+                    </div>
+
+                    <n-button :loading="isloading" type="primary" size="small" class="ml-16 rounded px-16"
+                        @click="handleClick()">
+                        <template #icon>
+                            <i class="okrfont" v-if="!(isloading)">&#xe72a;</i>
+                        </template>
+                        {{ $t('搜索') }}
+                    </n-button>
+                </div>
+                <div class="flex items-center mb-12 justify-between md:justify-start 2xl:justify-end 2xl:mb-0 ">
+                    <n-checkbox v-model:checked="completednotrated" class="rounded whitespace-nowrap mb-2 "
+                        @click="handleClick()">
+                        <span class="text-text-li">{{ $t('已评分未复盘') }}</span>
+                    </n-checkbox>
+                    <div @click="active = true" class="flex items-center md:hidden text-14"
+                        :class="searchActive ? 'text-primary-color' : 'text-text-tips'">
+                        <i class="okrfont text-18 mr-4">&#xe700;</i>
+                        {{ $t('筛选') }}
+                    </div>
+
+                </div>
+            </div>
             <div>
                 <OkrNotDatas v-if="items.length == 0 && !loadIng && !onscrolloading" :loadIng="loadIng" :msg="$t('暂无复盘')"
                     :types="searchObject != ''"></OkrNotDatas>
@@ -36,6 +97,63 @@
                     </div>
                 </div>
             </div>
+            <n-drawer v-model:show="active" default-height="362px" placement="bottom" resizable>
+
+                <n-drawer-content class="screen-d">
+                    <template #header>
+                        <div class="flex w-full items-center justify-between text-text-li text-16 md:text-18">
+                            {{ $t('筛选') }}
+                            <i class="okrfont text-text-tips" @click="active = false">&#xe6e5;</i>
+                        </div>
+                    </template>
+                    <div class="flex flex-col h-full">
+                        <div v-if="userInfo == 'admin'" class="whitespace-nowrap text-text-li mb-4">
+                            {{ $t('部门') }}
+                        </div>
+                        <n-select v-if="userInfo == 'admin'" v-model:value="departmentsvalue" :options="departments"
+                            clearable class="mr-24" :placeholder="$t('全部')" />
+
+                        <div class=" whitespace-nowrap text-text-li mb-4" :class="userInfo == 'admin' ? 'mt-16' : ''">
+                            {{ $t('负责人') }}
+                        </div>
+                        <n-select v-model:value="principalvalue" :options="principal" :on-search="getUser" class=""
+                            filterable :placeholder="$t('全部')" clearable>
+                            <template #action>
+                                <div v-if="principallast_page > principalpage" quaternary
+                                    class=" h-full w-full whitespace-nowrap text-center" @click.stop="principalClick('')">
+                                    {{ $t('更多...') }}
+                                </div>
+                                <div v-else quaternary class=" h-full w-full whitespace-nowrap text-center">
+                                    {{ $t('已经到底了') }}
+                                </div>
+                            </template>
+                        </n-select>
+
+                        <div class="mt-16 whitespace-nowrap mb-4 text-text-li">
+                            {{ $t('时间') }}
+                        </div>
+                        <div v-if="showDatePickers" class="okr-date-picker-waps">
+                            <DatePickers />
+                        </div>
+                        <n-date-picker v-else class="h-[36px]" v-model:value="daterange" value-format="yyyy.MM.dd HH:mm:ss"
+                            type="daterange" clearable size="medium" />
+
+
+                        <div
+                            class="flex justify-between gap-4 mt-auto border-solid pt-12 border-0  border-t-[1px] border-[#F2F2F2]">
+                            <n-button :loading="isloading" strong secondary type="tertiary" class="flex-1"
+                                @click="handleReset">
+                                {{ $t('重置') }}
+                            </n-button>
+                            <n-button :loading="isloading" type="primary" class="flex-1" @click="handleClick()">
+                                {{ $t('搜索') }}
+                            </n-button>
+                        </div>
+
+                    </div>
+                </n-drawer-content>
+            </n-drawer>
+
             <OkrLoading v-if="onscrolloading" position='onscroll'></OkrLoading>
             <!-- OKR详情 -->
             <OkrDetailsModal ref="RefOkrDetails" :id="detailId" :show="okrDetailsShow" @openDetail="openOkrDetail" @close="() => {
@@ -51,19 +169,44 @@ import { ref } from "vue"
 import OkrReplayDetail from "@/views/components/OkrReplayDetails.vue"
 import OkrNotDatas from "@/views/components/OkrNotDatas.vue"
 import { getReplayList } from '@/api/modules/replay'
+import { getDepartmentList } from '@/api/modules/department'
 import OkrDetailsModal from '@/views/components/OkrDetailsModal.vue';
 import OkrLoading from '../components/OkrLoading.vue'
 import { GlobalStore } from '@/store';
+import { UserStore } from '@/store/user'
+import { getUserList } from '@/api/modules/created'
+import utils from "@/utils/utils"
 
 const { proxy } = getCurrentInstance();
-
+const datePickerApps = ref([])
 const globalStore = GlobalStore()
 const items = ref([])
 const loadIng = ref(false)
+const isloading = ref(false)
 const onscrolloading = ref(false)
 const page = ref(1)
 const last_page = ref(99999)
 
+const active = ref(false)
+
+const keyWord = ref('')
+
+const departmentsvalue = ref(null)
+const departments = ref([
+    { label: $t('全部'), value: null },
+])
+
+const principal = ref([])
+const principalvalue = ref(null)
+const principallast_page = ref(99999)
+const principalpage = ref(1)
+
+const userInfo = UserStore().info.identity[0]
+const showDatePickers = computed(() => window.__MICRO_APP_BASE_APPLICATION__ ? 1 : 0)
+
+const daterange = ref<[number, number]>([0, 0])
+
+const completednotrated = ref(false)
 
 const okrDetailsShow = ref(false)
 const detailId = ref(0)
@@ -75,6 +218,11 @@ const props = defineProps({
         default: "",
     }
 })
+
+const searchActive = computed(() => {
+    return departmentsvalue.value != null || principalvalue.value != null || daterange.value[0] != 0 || daterange.value[1] != 0
+})
+
 
 watch(() => props.searchObject, (newValue) => {
     clearInterval(searchTime.value)
@@ -98,7 +246,12 @@ const getData = (type) => {
     if (last_page.value >= page.value || serstatic) {
         // 获取复盘列表
         const data = {
-            objective: props.searchObject,
+            select_replayed: completednotrated.value ? 1 : 0,//完成未完成
+            department_id: departmentsvalue.value,//部门
+            start_at: daterange.value[0] ? utils.TimeHandle(daterange.value[0]) : '',//开始时间
+            end_at: daterange.value[1] ? utils.TimeHandle(daterange.value[1], 2) : '',//结束时间
+            userid: principalvalue.value,//用户
+            objective: props.searchObject,//关键词
             page: page.value,
             page_size: 20,
         }
@@ -110,6 +263,7 @@ const getData = (type) => {
         getReplayList(data).then(({ data }) => {
             onscrolloading.value = false
             loadIng.value = false
+            isloading.value = false
             if (serstatic) {
                 data.data ?
                     items.value = data.data
@@ -123,6 +277,47 @@ const getData = (type) => {
             last_page.value = data.last_page
         })
     }
+}
+
+const init = () => {
+    principalClick('init')
+    getDepartmentList().then(({ data }) => {
+        data.data.map(item => {
+            departments.value.push({
+                label: item.name,
+                value: item.id,
+            })
+        })
+    })
+}
+
+//负责人
+const principalClick = (type) => {
+    if (type == 'init') {
+        principalpage.value = 1
+        principal.value = ([
+            { label: $t('全部'), value: null }
+        ])
+    } else {
+        principalpage.value++
+    }
+    const sendata = {
+        dept_only: userInfo == 'admin' ? false : true,
+        page: principalpage.value,
+        page_size: 20,
+        keyword: keyWord.value,
+    }
+    getUserList(sendata).then(({ data }) => {
+        if (data.data) {
+            data.data.map(item => {
+                principal.value.push({
+                    label: item.nickname,
+                    value: item.userid,
+                })
+            })
+        }
+        principallast_page.value = data.last_page
+    })
 }
 
 const onScroll = (e) => {
@@ -163,9 +358,127 @@ const resetGetList = () => {
     page.value = 1
     getData('search')
 }
+// 获取用户
+const getUser = (keyword) => {
+    if (keyword == '') {
+        principalpage.value = 1
+    }
+    keyWord.value = keyword
+    const sendata = {
+        dept_only: userInfo == 'admin' ? false : true,
+        page: principalpage.value,
+        page_size: 20,
+        keyword: keyword,
+    }
+
+    getUserList(sendata).then(({ data }) => {
+        if (keyword == '') {
+            principal.value = ([
+                { label: $t('全部'), value: null }
+            ])
+            data.data.map(item => {
+                principal.value.push({
+                    label: item.nickname,
+                    value: item.userid,
+                })
+            })
+        }
+        else {
+            principal.value = ([])
+            if (data.data) {
+                data.data.map(item => {
+                    principal.value.push({
+                        label: item.nickname,
+                        value: item.userid,
+                    })
+                })
+            }
+        }
+        principallast_page.value = data.last_page
+    })
+
+}
+
+
+//搜索
+const handleClick = () => {
+    isloading.value = true
+    page.value = 1
+    getData('search');
+}
+
+//重置
+const handleReset = () => {
+    departmentsvalue.value = null
+    principalvalue.value = null
+    daterange.value = [0,0]
+    active.value = false
+    page.value = 1
+    getData('search');
+}
+
+// 加载时间组件
+const loadDatePickers = () => {
+    nextTick(() => {
+        if (!window.Vues || !showDatePickers) return false;
+        unmountDatePickerApps()
+        document.querySelectorAll('datepickers').forEach(e => {
+            let app = new window.Vues.Vue({
+                el: document.querySelector('DatePickers'),
+                store: window.Vues.store,
+                render: (h: any) => {
+                    return h(window.Vues?.components?.DatePicker, {
+                        class: "okr-app-date-pickers",
+                        props: {
+                            editable: false,
+                            placeholder: $t("请选择时间"),
+                            format: "yyyy-MM-dd",
+                            type: "daterange",
+                            placement: "bottom-end",
+                            confirm: true,
+                            options: { shortcuts: window.$A ? window.$A.timeOptionShortcuts() : [] }
+                        },
+                        on: {
+                            "on-change": (e: any) => {
+                                daterange.value = e
+                            }
+                        }
+                    })
+                }
+            });
+            datePickerApps.value.push(app);
+        })
+    })
+}
+// 卸载时间组件
+const unmountDatePickerApps = () => {
+    if (datePickerApps.value) {
+        datePickerApps.value.forEach(app => {
+            app.$el.replaceWith(document.createElement("DatePickers"));
+            app.$destroy()
+        })
+        datePickerApps.value = [];
+    }
+}
+
+// 重载
+window.addEventListener('resize', function () {
+    loadDatePickers()
+})
+
+// 卸载
+window.addEventListener('apps-unmount', function () {
+    unmountDatePickerApps()
+})
+
+onBeforeUnmount(() => {
+    unmountDatePickerApps()
+})
 
 onMounted(() => {
+    init()
     getData('search')
+    loadDatePickers()
 })
 defineExpose({
     resetGetList,
@@ -230,4 +543,15 @@ defineExpose({
 
 .span-3 {
     @apply bg-[#72A1F7];
-}</style>
+}
+:deep(.n-drawer-header__main) {
+    @apply flex-1;
+}
+:deep(.n-checkbox-box__border) {
+    @apply rounded !important;
+}
+
+:deep(.n-checkbox .n-checkbox-box) {
+    @apply rounded !important;
+}
+</style>
