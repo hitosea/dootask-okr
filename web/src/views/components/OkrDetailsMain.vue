70785c2bdf490e9af58c8aae735e7027b2d66e30
kuaifan <aipaw@live.cn>
2025-05-29 01:25:08 +0800

    fix: 详情页被挤压

--- web/src/views/components/OkrDetailsMain.vue
+++ web/src/views/components/OkrDetailsMain.vue
@@ -3,9 +3,9 @@
         class="md:min-h-[640px] flex flex-col h-full md:flex-row max-md:fixed max-md:inset-0 max-md:pb-16 max-md:overflow-auto"
         :class="single ? [] : ['md:max-h-[640px]', 'md:h-auto']">
 
-        <div class="bg-[#FAFAFA]" style="height:var(--status-bar-height,0)"></div>
+        <div class="shrink-0 sticky top-0 bg-[#FAFAFA] z-[5] h-[var(--status-bar-height,0)]"></div>
 
-        <div v-if="!single" class="hidden max-md:flex flex-shrink-0 items-center justify-between sticky top-0 h-[52px] px-12 bg-[#FAFAFA] z-[5]">
+        <div v-if="!single" class="hidden max-md:flex flex-shrink-0 items-center justify-between sticky top-[var(--status-bar-height,0)] h-[52px] px-12 bg-[#FAFAFA] z-[5]">
             <i @click="handleReturn" class="okrfont text-26 text-text-tips z-[2]">&#xe676;</i>
             <h2 class="absolute left-0 right-0 text-center text-title-color text-17 font-medium">OKR {{ $t('详情') }}</h2>
             <n-popover placement="bottom-end" :show="showPopover" :z-index="modalZIndex" @clickoutside="showPopover = false">
